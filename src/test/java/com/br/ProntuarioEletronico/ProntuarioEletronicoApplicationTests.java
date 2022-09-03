185fba6b98302eb2efb6bd0606ad8356a71cd84f
narinsss <maarib03@gmail.com>
2022-09-02 23:19:56 -0300

    projeto spring

---
+++ src/test/java/com/br/ProntuarioEletronico/ProntuarioEletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ProntuarioEletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProntuarioEletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
